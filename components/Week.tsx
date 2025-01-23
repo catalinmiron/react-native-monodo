ab86cef966a3da4ad40395dc263cc393eae3c83b
catalinmiron <mironcatalin@gmail.com>
2025-01-23 13:13:02 +0100

    Add todo for handling the keyboard with gestures

--- components/Week.tsx
+++ components/Week.tsx
@@ -5,6 +5,7 @@ import { KeyboardAwareScrollView } from "react-native-keyboard-controller";
 import { Day } from "./Day";
 
 export function Week() {
+  // To handle gestures, check https://github.com/kirillzyusko/react-native-keyboard-controller/blob/main/example/src/screens/Examples/InteractiveKeyboard/index.tsx#L90
   return (
     <KeyboardAwareScrollView
       bottomOffset={62}
