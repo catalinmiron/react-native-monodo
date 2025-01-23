d612ccbdd614b595059ed0879b64f0a8b42ca619
catalinmiron <mironcatalin@gmail.com>
2025-01-23 11:48:07 +0100

    Add lucide icons + svg + nativewind interop for them

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,16 +1,20 @@
 import type { Todo } from "@/utils/mock";
+import { Check } from "lucide-react-native";
 import { Text, View } from "react-native";
 
 export function Todo({ todo }: { todo: Todo }) {
   return (
     <View className='flex-row gap-2 items-center'>
       <View
-        className={`w-4 h-4 border rounded-sm ${
+        className={`w-4 h-4 border rounded-sm items-center justify-center ${
           todo.done
             ? "bg-selected border-selected"
             : "border-black bg-transparent"
-        }`}
-      />
+        }`}>
+        {todo.done && (
+          <Check size={10} absoluteStrokeWidth className='stroke-white' />
+        )}
+      </View>
       <Text
         className={`font-barlow-400 ${
           todo.done ? "line-through decoration-selected" : ""
