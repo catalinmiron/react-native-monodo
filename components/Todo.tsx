cd3f8dd5ce4d730feb9238a3460a768d8764b086
catalinmiron <mironcatalin@gmail.com>
2025-01-24 11:42:39 +0100

    Toggle done on todo when pressing

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,10 +1,22 @@
+import { db } from "@/db/init";
 import { todos } from "@/db/schema";
+import { eq } from "drizzle-orm";
 import { Check } from "lucide-react-native";
 import { Text, View } from "react-native";
 
 export function Todo({ todo }: { todo: typeof todos.$inferSelect }) {
   return (
-    <View className='flex-row gap-2 items-center'>
+    <View
+      className='flex-row gap-2 items-center'
+      onTouchStart={() => {
+        console.log({ todo });
+        db.update(todos)
+          .set({
+            done: Boolean(todo.done) ? 0 : 1,
+          })
+          .where(eq(todos.id, todo.id))
+          .execute();
+      }}>
       <View
         className={`w-4 h-4 border rounded-sm items-center justify-center ${
           Boolean(todo.done)
